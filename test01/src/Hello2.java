3d412ec85602dfbe069ebf1b2c5279e896a3a8f8
kisski11 <kisskill11@nate.com>
2016-04-20 15:22:21 +0900

    add

--- test01/src/Hello2.java
+++ test01/src/Hello2.java
@@ -4,7 +4,8 @@ public class Hello2 {
   public static void main(String[] args) {
     System.out.println("Hello! ");
     System.out.println("okok! ");
-
+    System.out.println("nono! ");
+    System.out.println("oooo! ");
   }
 
 }
